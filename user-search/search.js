14b7d796122429ff543edafc6b1837358d7073c2
PatrickWacholtz04 <157766166+PatrickWacholtz04@users.noreply.github.com>
2025-01-30 21:59:11 +0000

    Start implementing api functionality. Unfinished

--- user-search/search.js
+++ user-search/search.js
@@ -1,3 +1,20 @@
+function loadApiData() {
+    let api = {}
+    fetch('../secret/api.json')  // Path to api.json file
+        .then(response => response.json())  // Parse the JSON response
+        .then(data => {
+            // Store the apiKey and apiUrl in temporary variables
+            const api_key = data.API_KEY;
+            const api_url = data.user_url;
+            capi = {key: api_key, url: api_url};
+        })
+        .catch(error => {
+            console.error('Error loading the JSON file:', error);
+        });
+    
+}
+
+
 function search() {
     // Check inputs in order of precedence given as id, email, name
     const id_input = document.getElementById("id-input");
@@ -5,6 +22,7 @@ function search() {
     const name_input = document.getElementById("name-input");
     // Find which value we should search by in order
     if (id_input.value.trim() != "") { // If input has a value
+        get_user_info('i', id_input.value);
         id_input.value = "Got the id";
     }
     else if (email_input.value.trim() != "") {
@@ -13,5 +31,17 @@ function search() {
     else if (name_input.value.trim() != "") {
         name_input.value = "get the name"
     }
+
+}
+
+// Function to fetch relevant user info from the provided information
+    // Flag: char, {i, e, n}
+    // Content: string, {user input}
+function get_user_info(flag, content) {
+    api = loadApiData();
+    console.log(api.key, api.url);
+
+    console.log(`Begin user search with specification ${flag}:${content}`);
+
 
 }
