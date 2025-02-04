c497e57153fda1076c7c818024356e296d3250ef
PatrickWacholtz04 <157766166+PatrickWacholtz04@users.noreply.github.com>
2025-02-04 14:42:24 +0000

    Work on implementing the API functionality

--- user-search/search.js
+++ user-search/search.js
@@ -1,28 +1,37 @@
-// function loadApiData() {
-//     let api = {}
-//     fetch('../secret/api.json')  // Path to api.json file
-//         .then(response => response.json())  // Parse the JSON response
-//         .then(data => {
-//             // Store the apiKey and apiUrl in temporary variables
-//             const api_key = data.API_KEY;
-//             const api_url = data.user_url;
-//             capi = {key: api_key, url: api_url};
-//         })
-//         .catch(error => {
-//             console.error('Error loading the JSON file:', error);
-//         });
+async function loadApiData(endpoint) {
+    try {
+        const response = await fetch('../secret/api.json');
+        const data = await response.json();
+        
+        api_key = data.API_KEY;
+
+        if (endpoint == "base") {
+            api_url = data.base_url;
+        }
+        else if (endpoint == "hardware") {
+            api_url = data.hardware_url;
+        }
+        else if (endpoint == "user") {
+            api_url = data.user_url;
+        }
+        else {
+            api_url = undefined;
+        }
+        return {key: api_key, url: api_url};
+    } catch (error) {
+        console.error(error);
+    }
     
-// }
+}
 
-
-function search() {
+async function search() {
     // Check inputs in order of precedence given as id, email, name
     const id_input = document.getElementById("id-input");
     const email_input = document.getElementById("email-input");
     const name_input = document.getElementById("name-input");
     // Find which value we should search by in order
     if (id_input.value.trim() != "") { // If input has a value
-        // get_user_info('i', id_input.value);
+        get_user_info('i', id_input.value);
         id_input.value = "Got the id";
     }
     else if (email_input.value.trim() != "") {
@@ -65,9 +74,46 @@ function result_populate(search_results) {
 // Function to fetch relevant user info from the provided information
     // Flag: char, {i, e, n}
     // Content: string, {user input}
-// function get_user_info(flag, content) {
-//     api = loadApiData();
-//     console.log(api.key, api.url);
+async function get_user_info(flag, content) {
+    console.log(`Begin user search with specification ${flag}:${content}`);
+    
+    // Load the api key and url
+    api = await loadApiData("user");
+    console.log("Getting API settings");
 
-//     console.log(`Begin user search with specification ${flag}:${content}`);
-// }
+    // Generate the endpoint based on the user's inputs
+    let endpoint = undefined;
+    if (flag =='i') {
+        endpoint = `?employee_num=${content}`;
+    }
+    else if (flag == "e") {
+        endpoint = `?email=${content}`;
+    }
+    else if (flag == 'n') {
+        // Split content from 'first last' to 'first' and 'last'
+        const names = content.split(" ");
+        const first_name = names[0];
+        const last_name = names[1];
+        endpoingt =`?first_name=${first_name}&last_name=${last_name}`;
+    }
+    else {
+        // Quit out of invalid flag was passed since no endpoint can be made
+        console.log("Unknown flag passed to get_user_info()");
+        return;
+    }
+
+    // Make the API fetch request
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer ' + api.key
+        }
+      };
+
+    fetch(api.url + endpoint, options)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+    
+}
